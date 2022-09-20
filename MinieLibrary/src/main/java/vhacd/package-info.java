6ea202cafa99b8efdb547b979b2bdffd9b0e1071
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2022-09-20 19:18:04 -0400

    Absolutely gut project

--- MinieLibrary/src/main/java/vhacd/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Java API for Khaled Mamou’s Volumetric-Hierarchical Approximate Convex
- * Decomposition (V-HACD) algorithm.
- */
-package vhacd;
--- MinieLibrary/src/main/java/vhacd4/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Java API for version 4 of Khaled Mamou’s Volumetric-Hierarchical Approximate
- * Convex Decomposition (V-HACD) algorithm.
- */
-package vhacd4;
